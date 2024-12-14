285b41daa7930fe8ffa4794f44c3097d684034b8
Dima <d.gotonoaga@outlook.com>
2024-12-14 20:22:44 +0200

    small updates

---
+++ src/Components/Weather.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import "./Weather.css";
+import search_icon from "../Assets/search.png";
+import clear_icon from "../Assets/clear.png";
+import cloud_icon from "../Assets/cloud.png";
+import drizzle_icon from "../Assets/drizzle.png";
+import humidity_icon from "../Assets/humidity.png";
+import rain_icon from "../Assets/rain.png";
+import snow_icon from "../Assets/snow.png";
+import wind_icon from "../Assets/wind.png";
+
+
+const Weather = () => {
+  const [inputValue, SetInputValue] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  };
+
+  const search = async (city) => {
+    if (!city) {
+      alert(`Please Enter a City Name`);
+      setWeatherData(null);
+      return;
+    }
+    try {
+      const apiKey = process.env.REACT_APP_API_KEY;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (!response.ok) {
+        alert(data.message);
+        return;
+      }
+
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon,
+      });
+    } catch (error) {
+      setWeatherData(null);
+      console.error("Error in fetching weather data!");
+    }
+  };
+
+  // useEffect(() => {
+  //   search("Bucharest");
+  // }, []);
+
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input
+          value={inputValue}
+          onChange={(e) => {
+            SetInputValue(e.target.value);
+          }}
+          type="text"
+          placeholder="Search"
+        />
+        <img src={search_icon} alt="" onClick={() => search(inputValue)} />
+      </div>
+      {weatherData ? (
+        // If weather data is available, show it
+        <>
+          <img
+            src={weatherData.icon}
+            alt="Weather icon"
+            className="weather-icon"
+          />
+          <p className="temperature">{weatherData.temperature}°C</p>
+          <p className="location">{weatherData.location}</p>
+          <div className="weather-data">
+            <div className="col">
+              <img src={humidity_icon} alt="Humidity" />
+              <div>
+                <p>{weatherData.humidity} %</p>
+                <span>Humidity</span>
+              </div>
+            </div>
+            <div className="col">
+              <img src={wind_icon} alt="Wind Speed" />
+              <div>
+                <p>{weatherData.windSpeed} m/s</p>
+                <span>Wind Speed</span>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        // If no weather data, show placeholder message
+        <p>Please enter a city to get weather information</p>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
